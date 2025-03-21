7703abedeb2feb393873acdef8e4cfdb4ed7f9e8
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-22 02:38:55 +0600

    page42 practice problem solved

---
+++ variables/page42.js
@@ -0,0 +1,49 @@
+// 7 practice problem
+
+// ১. ধর তোর বন্ধু পরীক্ষায় বাম্বু খাইছে। এখন সে হ্যাপি কি না, সেটা একটা boolean টাইপের ভেরিয়েবল ডিক্লেয়ার করে সেটার মান রাখ। আর সেটা console-এ প্রিন্ট কর।
+let isBamboo = true
+console.log('my friend got bamboo',isBamboo)
+
+
+
+
+// ২. তুই নিজে একটা প্রোগ্রাম লিখে দেখ, তোর crush তোকে মেসেজ দিয়েছে কি না। ধর মেসেজ দিলে true, না দিলে false। সেটা প্রিন্ট কর console.log() দিয়ে।
+let crush = true
+console.log('crush message',crush)
+
+
+
+
+// ৩. তোর প্রিয় টিম খেলায় জিতে গেল, এখন সেই খুশিতে বগল বাজাতে বাজাতে celebrating নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার করে সেটার মান দে, আর আউটপুট দেখ।
+let celebrating = true
+console.log('Celebrating the win',celebrating)
+
+
+
+
+// ৪. তোর এখন boolean টাইপ সম্পর্কে ভালো ধারণা হয়েছে। এবার একটা প্রোগ্রাম লিখ, যেখানে তুই তিনটা boolean ভেরিয়েবল ডিক্লেয়ার করবি: "isHappy", "hasMoney", আর "isHungry"। এই তিনটার মান যথাক্রমে true, false, আর true সেট করবি। তারপর আউটপুট দে console.log() দিয়ে।
+const isHappy = true
+const hasMoney = false
+const isHungry = true
+console.log(isHappy, hasMoney, isHungry)
+
+
+
+
+// ৫. তোর আজকে পড়া শেষ হয়েছে কি না? পড়া শেষ হলে true, না হলে false হবে। আর সেটা রাখার জন্য isStudyDone নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার কর।
+let isStudyDone = true
+console.log( 'Today study done',isStudyDone)
+
+
+
+
+// ৬. এখন isPhoneCharged নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার কর। চার্জ শেষ হলে true, শেষ না হলে false।
+let isPhoneCharged = false
+console.log( 'phone charged',isPhoneCharged)
+
+
+
+
+// ৭. তোর কি রাতের খাওয়া শেষ? সেটা নিয়ে isDinnerDone নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার কর। খাওয়া শেষ হলে কী লিখবি, আর শেষ না হলে কী লিখবি, সেটা তো জানস অলরেডি।
+let isDinnerDone = true
+console.log('dinner done',isDinnerDone)
