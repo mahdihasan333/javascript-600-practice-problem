fa94587a532b2a991264dd85acbe702c378cab06
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-22 02:09:38 +0600

    page41 practice problem solved

---
+++ variables/page41.js
@@ -0,0 +1,47 @@
+// 7 practice problem
+
+// ১. এখন name নামে একটা ভেরিয়েবল ডিক্লেয়ার কর, আর যেটার মান হবে তোর নাম। এরপর সেটাকে console-এ আউটপুট হিসেবে দেখা।
+const name = 'mahdi hassan'
+console.log(name)
+
+
+
+
+// ২. তোর ঠিকানা (address) একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করে দেখ। আর সেটাকে console.log() দিয়ে আউটপুট দেখ।
+const address = 'Lakshmipur, Bangladesh'
+console.log(address)
+
+
+
+
+// ৩. তোর বেস্ট ফ্রেন্ড কোন স্কুল, কলেজ বা ভার্সিটিতে পড়ে, সেই স্কুল, কলেজ বা ভার্সিটির নামে একটা ভেরিয়েবল ডিক্লেয়ার কর। তারপর সেটাকে আউটপুট হিসেবে দেখা।
+const bestFriendCollege = 'Brilliant Polytechnic College';
+console.log(bestFriendCollege);
+
+
+
+
+// ৪. তোর প্রিয় নায়িকার নাম fan নামে একটা ভেরিয়েবলে রাখ। তারপর কনসোলে দেখা।
+const fan = 'Sai Pallavi'
+console.log(fan)
+
+
+
+
+// ৫. এই বছরের তোর প্রিয় সিনেমার নাম movieOfTheYear নামে একটা ভেরিয়েবলে রাখ। তারপর সেটা কনসোলে প্রিন্ট কর।
+const movieOfTheYear = 'Daagi'
+console.log(movieOfTheYear)
+
+
+
+
+// ৬. সবার কিছু শখ বা hobby থাকে। তোর শখ বা hobby নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর।
+const hobby = 'Book Reading'
+console.log(hobby)
+
+
+
+
+// ৭. তোর প্রিয় রং কোনটা? color নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর।
+const color = 'White'
+console.log(color)
