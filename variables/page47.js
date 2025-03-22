69cba40037cfb3cfa44f3f3b75e86463f1cdcc94
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-22 23:47:45 +0600

    page47 practive problem solved

---
+++ variables/page47.js
@@ -0,0 +1,43 @@
+// 10 practice problem
+
+// ১. ধর, তুই 100 টাকা আয় করেছিস। তার মধ্যে 40 টাকা খরচ করছিস। বাকি টাকা কত রইল, সেটা বের করার একটা প্রোগ্রাম লিখ।
+const earningMoney = 100
+const spentMoney = 40
+const remainingMoney = earningMoney - spentMoney
+console.log(remainingMoney)
+
+
+
+
+// ২. তোর কাছে 10টা পেন্সিল আছে। প্রতিবার তুই একসাথে ২টা পেন্সিল ব্যবহার করবি। কত বার ব্যবহার করতে পারবি? প্রোগ্রাম লিখে বের কর।
+const totalPencils = 10
+const pencilsUse = 2
+const totalUsesPencils = totalPencils / pencilsUse
+console.log(totalUsesPencils)
+
+
+
+// ৩. তোর কাছে 60 টাকা আছে। তুই যদি প্রতিবার 15 টাকা করে খরচ করিস, কয়বার খরচ করতে পারবি? এবং খরচের পর কয় টাকা বাকি থাকবে, সেটা বের করতে একটা প্রোগ্রাম লিখ।
+const totalMoneys = 60
+const moneyPerUse = 15
+const totalUses = totalMoneys / moneyPerUse
+console.log(totalUses)
+
+
+
+// ৪. ধর, তুই দুইটা স্ট্রিং লিখলি "Hello" আর "World"। এই দুইটা স্ট্রিংকে একসাথে জোড়া দিয়ে কীভাবে "Hello World" বানানো যায়, সেটা দেখানোর জন্য একটা প্রোগ্রাম লিখ।
+const firstString = 'Hello'
+const secondString = 'World'
+const connectedString = firstString + secondString
+console.log(connectedString)
+
+
+
+// ৫. তুই যদি 153 কেজি চাল আর 261 কেজি ডাল কিনলি, তাহলে তুই কয় কেজি জিনিস কিনেছিস? মোট কেজি বের করার একটা প্রোগ্রাম লিখ।
+const rice = 153
+const dal = 261
+const totalKg = rice + dal
+console.log(totalKg)
+
+
+
