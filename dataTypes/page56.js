08a7b6c5a13bd1049da1a47ada00147bdb3c9930
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-24 16:49:22 +0600

    page56 practice problem solved

---
+++ dataTypes/page56.js
@@ -0,0 +1,60 @@
+// 7 practice problem
+
+// ১. তুই score নামে একটা ভেরিয়েবল লিখ। এখন এইটা কীসের স্কোর, সেটা ভেরিয়েবলের নাম দেখে বোঝা যাচ্ছে না। তাই এই ভেরিয়েবল ডিক্লেয়ার করার ওপরে এক লাইন কমেন্ট লিখ। কমেন্টে থাকবে, এইটা কীসের স্কোর।
+// This is Hyderabad's first match run of 2025.
+const score = 287
+
+
+
+
+// ২. এখন radius নামে একটা ভেরিয়েবল লিখ। তারপর এইটা কীসের রেডিয়াস সেটা বোঝানোর জন্য তিন লাইনের কমেন্ট লিখ। প্রত্যেক লাইনের আগে স্ল্যাশ চিহ্ন দিয়ে কমেন্ট লিখবি।
+// This is the radius of a circle.
+// It is measured from the center to the edge.
+// we can use it to calculate area and circumference.
+let radius = 5
+
+
+
+
+// ৩. তোর পছন্দের জামা কয়টা, সেটা নিয়ে dress নামে একটা ভেরিয়েবল ডিক্লেয়ার কর। তারপর সেটার ওপর মাল্টিলাইন কমেন্ট লিখ স্ল্যাশ এবং এস্টেরিস্ক চিহ্ন দিয়ে।
+/*
+I have 5 favorite shirts.
+These are my favorite shirts that I wear most of the time.
+Each shirt has a different color and design, and I feel very comfortable wearing them.
+*/
+const dress = 7
+
+
+
+// ৪. তোকে একটা গিফট হিসেবে 100 টাকা দেয়া হয়েছে, giftMoney নামে একটা ভেরিয়েবলে রাখ। তারপর কমেন্টে লিখবি, এই টাকা কী কারণে দেয়া হয়েছে।
+// This is my Eid gift money
+const giftMoney = 100
+
+
+
+
+// ৫. তুই ঠিক করলি 7 ঘণ্টা পড়ালেখা করবি। studyTime নামে একটা ভেরিয়েবলে রাখ। তার ওপর চার-পাঁচ লাইনের কমেন্ট লিখ, এই 7 ঘণ্টার মধ্যে কোন কোন সাবজেক্টের কী কী জিনিস তুই কতক্ষণ ধরে পড়বি।
+/*
+I have planned to study for 7 hours daily for my classes.
+Al-Quran Studies: 2 hours (Tafsir and Hadith studies)
+Arabic Language: 1.5 hours (Grammar - Nahw & Sarf practice)
+Islamic Fiqh: 1.5 hours (Jurisprudence & Usul al-Fiqh)
+Islamic History: 1 hour (Muslim world history & biographies of caliphs)
+Others: 1 hour (Notes preparation, assignments, and revision)
+*/
+const studyTime = 7
+
+
+
+
+// ৬. তোর মোবাইল পেমেন্টের জন্য sendMoney নামে একটা ভেরিয়েবল ডিক্লেয়ার কর। তার ওপর দুই লাইনের কমেন্ট লিখ, যাকে মানি সেন্ড করতেছস, সে কত টাকা পাবে, আর সার্ভিস ফি কত টাকা।
+// I am sending 1000 Taka, and a service fee of 20 Taka will be deducted.
+// So the recipient will receive 980 Taka.
+const sendMoney = 1000;
+
+
+
+
+// ৭. তুই অনলাইন থেকে একটা প্রোডাক্ট অর্ডার করছস। তোর কাছ থেকে মোট কত টাকা নিবে, সেটা pay নামে একটা ভেরিয়েবলে রাখ। তারপর ওপরে এক লাইন কমেন্ট লিখবি। এই টাকার মধ্যে প্রোডাক্টের দাম এবং ডেলিভারি ফি যোগ করা আছে।
+// Product Price = 1630 + Delivery Charge = 120
+const pay = 1750;
