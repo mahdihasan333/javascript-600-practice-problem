20fa398491c8ad3e475bdc747692dde9f793e448
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-25 17:57:24 +0600

    page59 practice problem solved

---
+++ dataTypes/page59.js
@@ -0,0 +1,52 @@
+// 7 practice problem
+
+// ১. তুই একটা প্রোগ্রাম লিখে দেখ, যেখানে তুই ভেরিয়েবল ডিক্লেয়ার করবি camelCase ব্যবহার করে। ধর, তুই তোর প্রিয় খাবার নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর। আর সেটার মান দে।
+const favoriteFood = "Biriyani"
+console.log(favoriteFood)
+
+
+
+// ২. তুই দেখ, ভেরিয়েবলের নাম case-sensitive কি না। একটা প্রোগ্রাম লিখে দুইটা ভেরিয়েবল ডিক্লেয়ার করবি, একটার নাম "city", আরেকটার নাম "City"। প্রথমটার মান "Chittagong" আর দ্বিতীয়টার মান "Dhaka" দে। console.log() দিয়ে দুইটা আউটপুট দেখ।
+const city = 'Chittagong'
+const City = 'Dhaka'
+console.log(city)
+console.log(City)
+
+
+
+
+// ৩. তুই userName নামে একটা ভেরিয়েবল ডিক্লেয়ার করবি এবং তার মান Johan দে। এবার username নামে আরেকটা ভেরিয়েবল ডিক্লেয়ার করবি এবং তার মান Jovan দে। দুইটা আউটপুট দেখ console.log() দিয়ে।
+const userName = 'Johan'
+const username = 'Jovan'
+console.log(userName)
+console.log(username)
+
+
+
+
+
+// ৪. আমার বাসার ঠিকানা (বাসরের ঠিকানা বলি নাই কিন্তু) নামে ইংরেজিতে একটা ভেরিয়েবল ডিক্লেয়ার করবি। camelCase ব্যবহার করে ভেরিয়েবল ডিক্লেয়ার করবি। তারপর এইটার স্ট্রিং একটা মান সেট করবি।
+const homeAddress = 'Lakshmipur'
+console.log(homeAddress)
+
+
+
+// ৫. সুপার ওভারে কত রান হইছে, সেটার জন্য super নামে একটা ভেরিয়েবল ডিক্লেয়ার কর এবং মান সেট কর। করার পর যে আউটপুট দেখতে পাচ্ছস, সেটা কি ঠিক আছে নাকি কোনো সমস্যা আছে।
+// SyntaxError দেখতে পাচ্ছি
+const super = 25
+console.log(super)
+
+
+
+// ৬. তুই কোন ক্লাসে পড়স, সেটা লেখার জন্য class নামে একটা ভেরিয়েবল লিখ। তারপর আউটপুটে দেখ, কী দেখায়।
+// SyntaxError দেখতে পাচ্ছি
+const class = 'honours'
+console.log(class)
+
+
+
+
+// ৭. সেকেন্ড পজিশনে কে ব্যাট করবে, সেটার জন্য 2ndPosition নামে একটা ভেরিয়েবল লিখে সেখানে একজন ব্যাটসম্যানের নাম লিখে দে। দেখ আউটপুট দেখায় কি না। যদি সমস্যা করে, তাহলে কী করা লাগে, সেটা কমেন্ট করে ভেরিয়েবলের ওপরে লিখ।
+// জাভাস্ক্রিপ্টে ভেরিয়েবল সংখ্যা দিয়ে শুরু করা যায় না, তাই "2ndPosition" কাজ করবে না। এখানে secondPosition এইভাবে দিতে হবে
+const 2ndPosition = 'Hashim Amla'
+console.log(2ndPosition)
