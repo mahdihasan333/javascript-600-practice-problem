124e31f85bccdeb457b1b66478917d871dda1c2a
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-27 15:25:21 +0600

    page70 practice problem solved

---
+++ comparison/page70.js
@@ -0,0 +1,62 @@
+// 8 practice problem
+
+// ১. তোর ছোট ভাইয়ের বয়স 10 বছর, আর তোর বয়স 15 বছর। একটা প্রোগ্রাম লিখে দেখ, তোর ছোট ভাইয়ের বয়স কি তোর বয়স থেকে কম বা সমান কি না।
+const myBrotherAge = 10
+const myAge = 15
+console.log(myBrotherAge <= myAge)
+
+
+
+
+// ২. একটা পরীক্ষায় পাস মার্কস 33। ধর, তুই 45 পেয়েছিস। চেক কর, তুই পাস করেছিস কি না।
+const passMarks = 33
+const obtainedMarks = 45
+console.log(passMarks < obtainedMarks)
+
+
+
+
+// ৩. দুইটা ভেরিয়েবলে জাম্বুরা আর কদবেলের দাম রাখা হলো। জাম্বুরা কি কদবেলের চেয়ে সস্তা কি না, সেটার তুলনা কর।
+const jambura = 15
+const kodbel = 10
+console.log(jambura < kodbel)
+
+
+
+
+// ৪. তুই 10টা বই পড়েছিস, আর তোর বন্ধু পড়ছে 10টা। চেক কর, তোরা কি সমান সংখ্যক বই পড়েছিস কি না।
+const myBooks = 10
+const myFriendBooks = 10
+console.log(myBooks == myFriendBooks)
+
+
+
+
+// ৫. তোর কাছে 15টা বিস্কুট আর তোর বন্ধুর কাছে 20টা। চেক কর, তুই কি তোর বন্ধুর থেকে কম বিস্কুট পেয়েছিস কি না।
+const myCookies = 15
+const myFriendCookies = 20
+console.log(myCookies < myFriendCookies)
+
+
+
+
+// ৬. একটা ফুটবল ম্যাচে রিয়াল মাদ্রিদ ২টা গোল করছে, আর বার্সা 3 টা গোল করছে। চেক কর, রিয়াল মাদ্রিদ কি বার্সার চেয়ে কম গোল করেছে কি না।
+const realMadrid = 2
+const barcelona = 3
+console.log(realMadrid < barcelona)
+
+
+
+
+// ৭. তোর নাম "Rahim", আর তোর বন্ধুর নাম "Karim"। চেক কর, তোর নাম কি তোর বন্ধুর নামের সমান কি না।
+const myName = "Rahim"
+const myFriendName = "Karim"
+console.log(myName == myFriendName)
+
+
+
+
+// ৮. তুই আজকে পড়াশোনা করেছিস (true), আর তোর বন্ধু পড়াশোনা করেনি (false)। চেক কর, তোরা কি একই রকম কাজ করেছিস কি না।
+const myStudy = true
+const myFriendStudy = false
+console.log(myStudy == myFriendStudy)
