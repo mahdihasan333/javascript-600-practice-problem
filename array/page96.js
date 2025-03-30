ba2b80b961f3e159ddc1d3ff1e33461962bc50ee
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-30 20:54:09 +0600

    page96 practice problem solved

---
+++ array/page96.js
@@ -0,0 +1,53 @@
+// 7 practice problem
+
+// ১. তুই তোর সব বন্ধুদের নাম একটা অ্যারেতে রাখাল। এখন তুই বের কর, 3 নাম্বার ইনডেক্সে কে আছে।
+const friends = ['Sihab', 'rahat', 'tarek', 'fahad', 'jibon', 'jahid', 'rabbi']
+const friend = friends[3]
+console.log(friend)
+
+
+
+
+// ২. তোর 7টা প্রিয় বইয়ের নাম একটা অ্যারেতে রাখ। তারপর দেখ, 5 নাম্বার পজিশনের বইয়ের নাম কী।
+const books = ['Atomic Habits', 'Deep Work', 'Rich Dad Poor Dad', 'The Alchemist', 'Ikigai', 'marhaba javascript a maro thaba', 'A Game of Thrones']
+const book = books[5]
+console.log(book)
+
+
+
+
+// ৩. তুই একটা অ্যারে বানালি, যেখানে 1 থেকে 10 পর্যন্ত সংখ্যাগুলো রাখলি। এখন 7 নাম্বার পজিশনের মানকে 30 দিয়ে আপডেট কর।
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers[7] = 30
+console.log(numbers)
+
+
+
+
+// ৪. তোর মোবাইলে কিছু গেম আছে এবং তুই সেগুলো একটা অ্যারেতে রাখলি- 'ফ্রি ফায়ার', 'পাবজি', 'ক্যান্ডি ক্রাশ', 'টেম্পল রান'। এখন তুই চাস, 2 নাম্বার ইন্ডেক্সের গেমটা পরিবর্তন করে সেখানে 'সাবওয়ে সাফার' রাখবি। একটা প্রোগ্রাম লিখে সেটাই কর।
+const games = ['free Fire', 'PUBG', 'Candy Crush', 'Temple Run']
+games[2] = 'Subway Surfers'
+console.log(games)
+
+
+
+
+// ৫. তোর পরিবারের সদস্যের নাম একটা অ্যারেতে রাখ। তারপর থার্ড পজিশনের সদস্যের নাম আউটপুট হিসেবে দেখা।
+const familyMembers = ['Mom', 'Dad', 'Brother', 'Grandfather', 'Grandmother']
+const familyMember = familyMembers[3]
+console.log(familyMember)
+
+
+
+// ৬. তুই প্ল্যান করলি 7টা দেশ ঘুরবি। দেশগুলোর নাম একটা অ্যারেতে রাখ। তারপর 4 নাম্বার পজিশনের দেশের নাম বের কর।
+const countries = ['India', 'USA', 'China', 'Japan', 'Germany', 'France', 'Thailand']
+const country = countries[4]
+console.log(country)
+
+
+
+
+// ৭. তোর পড়ার টেবিলে 4টা জিনিস আছে। সেগুলোর নাম একটা অ্যারেতে রাখ। তারপর 7 নাম্বার পজিশনের জিনিসটা কী, সেটি বের কর।
+const studyTableItems = ['Book', 'Laptop', 'pen', 'Notebook']
+const singleItem = studyTableItems[7]
+console.log(singleItem)
