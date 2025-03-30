9d79dc8a3a55b57a780c8c81014645c02c6f76bc
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-30 18:13:57 +0600

    page93 practice problem solved

---
+++ array/page93.js
@@ -0,0 +1,50 @@
+// 7 practice problem
+
+// ১. তুই একটা অ্যারে বানালি, যেখানে 71 থেকে 79 পর্যন্ত সংখ্যাগুলো রাখ।
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79]
+console.log(numbers)
+
+
+
+
+// ২. তোর বাসায় একটা ছোট ফ্রিজ আছে, যেখানে ঠিক 5 টা ফল রাখতে পারিস। তুই একটা অ্যারে বানিয়ে তাতে আপেল, কলা, কমলা, আম আর পেয়ারা রাখলি।
+const fruits = ['Apple', 'Banana', "Orange", "Mango", "Guava"]
+console.log(fruits)
+
+
+
+
+// ৩. ধর, তুই একদিন বাজারে গিয়েছিলি। তোর ব্যাগে আলু, পেঁয়াজ, বেগুন, মুলা, গাজর, টমেটো, ঢ্যাঁড়স, লাউ, শসা আর পটল 10টা সবজি রাখলি। একটা প্রোগ্রাম লিখে বের কর, ব্যাগে মোট কয়টা সবজি আছে।
+const vegetables = ['Potato', 'Onion', 'Eggplant', 'Radish', 'Carrot', 'Tomato', 'Okra', 'Bottle gourd', 'Cucumber', 'Pointed gourd']
+const size = vegetables.length
+console.log(size)
+
+
+
+
+// ৪. তোর 5 টা প্রিয় সিনেমার নাম একটা অ্যারেতে রাখ।
+const favoriteMovies = ['Surongo', 'Daagi', 'Kabir Singh', 'Animal', 'Pathan']
+console.log(favoriteMovies)
+
+
+
+
+// ৫. একটা অ্যারে লিখ, যেটার মধ্যে 11 থেকে 30-এর মধ্যে বিজোড় সংখ্যাগুলো থাকবে।
+const odd = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
+console.log(odd)
+
+
+
+
+// ৬. তোর চারপাশে তাকিয়ে যে যে কালার দেখতে পারতেছস, সেগুলোর একটা অ্যারে বানা। তারপর সেই অ্যারের লেন্থ কনসোল লগ কর।
+const color = ['Blue', 'Green', 'Yellow', 'White']
+const colorLength = color.length
+console.log(colorLength)
+
+
+
+
+// ৭. কয়েকটা দেশের রাজধানীর নাম নিয়ে একটা অ্যারেতে রাখ এবং তার লেন্থ কনসোল লগ কর।
+const countries = ['Dhaka', 'London', 'Tokyo', 'Paris', 'Berlin', 'Rome']
+const countryLength = countries.length
+console.log(countryLength)
