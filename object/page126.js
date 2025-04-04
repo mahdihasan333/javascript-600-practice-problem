a915c1af8c7d32aee9c064d4533ae39dd4db03be
Mahdi Hassan <mahdioffi333@gmail.com>
2025-04-04 21:26:20 +0600

    page-126 practice problem solved

---
+++ object/page126.js
@@ -0,0 +1,77 @@
+// 7 practice problem
+
+// ১. তুই teacher নামে একটা অবজেক্ট বানা। এইটার মধ্যে টিচারের নাম থাকবে। উনি কোন সাবজেক্ট পড়ান, সেটার নাম থাকবে; স্যারের বয়স, বাসা কোথায় এই টাইপের পাঁচটা জিনিস থাকবে।
+const teacher = {
+    name: 'H.M Alauddin',
+    subject: 'Bangla',
+    age: 45,
+    address: 'Lakshmipur, Bangladesh',
+    isMarried: true,
+}
+
+
+
+
+// ২. গাছ (tree) নামে একটা object তৈরি কর, যার মধ্যে থাকবে, এইটা কী গাছ, উচ্চতা কেমন, বয়স কত, ফলের নাম কী ইত্যাদি।
+const tree = {
+    name: 'Mango Tree',
+    height: '10 feet',
+    age: 15,
+    fruit: 'Mango',
+}
+
+
+
+
+// ৩. তোর আশেপাশে কোনো প্রাণী থাকলে (গরু, বিড়াল, কুকুর, ইঁদুর, তেলাপোকা, মশা, মাছি, পিঁপড়া, টিকটিকি, মানুষ বা অন্য কোনো প্রাণী থাকলে), সেটার জন্য একটা অবজেক্ট লিখে ফেল।
+const cat = {
+    color: 'White',
+    age: '2 years old',
+    gender: 'Male',
+    favoriteFood: 'Fish'
+}
+
+
+
+
+// ৪. তোর আব্বুর জন্য father নামে একটা অবজেক্ট বানিয়ে ফেল। সেখানে উনার নাম, পেশা, বয়স, সন্তানের সংখ্যা ইত্যাদি তথ্য দিয়ে ফেল।
+const father = {
+    name: 'Mr. Abdullah',
+    profession: 'Business',
+    age: 50,
+    numberOfChildren: 1
+}
+
+
+
+
+// ৫. একটা বাইক (motorbike) নিয়ে object বানা। সেখানে ব্রান্ডের নাম, টায়ারের সংখ্যা, রং, সর্বোচ্চ গতি আর দাম থাকবে।
+const motorbike = {
+    brand: 'Yamaha',
+    tires: 2,
+    color: 'Black',
+    maxSpeed: '250 km/h',
+    price: 250000
+}
+
+
+
+
+// ৬. একটা পাখি (bird) অবজেক্ট তৈরি কর, যেখানে পাখির নাম, রং, খাদ্য ইত্যাদি প্রোপার্টি থাকবে।
+const bird = {
+    name: 'Cockatiel',
+    color: 'White',
+    food: ['Grass', 'Leaves']
+}
+
+
+
+
+// ৭. একটা laptop অবজেক্ট তৈরি কর, যেখানে থাকবে ব্রান্ডের নাম, প্রসেসর, র‍্যাম, দাম, আর ডিসপ্লের সাইজ ইত্যাদি ডাটা থাকবে।
+const laptop = {
+    brand: 'Apple',
+    processor: '2.5 GHz Quad-Core Intel Core i7',
+    ram: '16GB 1600 MHz DDR3',
+    price: 150000,
+    screenSize: '15-inch'
+}
