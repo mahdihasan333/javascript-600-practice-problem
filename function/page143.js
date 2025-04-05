951dfaf54458bca2d0dd0cbbf34b390f061df333
Mahdi Hassan <mahdioffi333@gmail.com>
2025-04-05 16:37:38 +0600

    page 143 practice problem solved

---
+++ function/page143.js
@@ -0,0 +1,74 @@
+// 7 practice problem
+
+// ১. জুস খাও নামে একটা ফাংশন লিখ। যেটাকে কল করলে আউটপুট দিবে- এই নাও লেবুর জুস।
+function juiceKhaw () {
+    console.log('Here is your lemon juice')
+}
+juiceKhaw()
+
+
+
+
+// ২. এখন myRoutine নামে একটা ফাংশন লিখ। এইটার ভিতরে থাকবে, প্রতিদিন সকালে ঘুম থেকে উঠে তুই কী কী কাজ করস, সেগুলোর লিস্ট। একটা একটা করে কাজের নাম এক একলাইনে কনসোেল লগ দিয়ে লিখবি।
+function myRoutine () {
+    console.log('Woke up in the morning')
+    console.log('Prayed Fajr namaj')
+    console.log('Read Quran')
+}
+myRoutine();
+
+
+
+
+// ৩. তোর পরিবারের জন্য একটা ফাংশন লিখ। তুই যেকোনো একটা নাম দে। তারপর এইটার মধ্যে তোর যত পরিবারের সদস্য আছে, তাদের নাম এক এক করে আউটপুট করে দেখাবি।
+function myFamily () {
+    console.log('My Father Shah Alom')
+    console.log('My Mother Noeon Akter')
+    console.log('My self Md. Mahdi hassan Rafi')
+}
+myFamily()
+
+
+
+
+// ৪. একটা taskDone নামে ফাংশন লিখ। ফাংশনটাতে তোর সারাদিনের ৫টা কাজ একেক লাইনে কনসোেল লগ দিয়ে দেখা।
+function taskDone () {
+    console.log('Task 1: Prayed Fajr namaz')
+    console.log('Task 2: Recited Quran')
+    console.log('Task 3: Had breakfast')
+    console.log('Task 4: Went to Madrasa')
+    console.log('Task 5: Wrote code')
+}
+taskDone()
+
+
+
+// ৫. greetMe নামে ফাংশন লিখ। ফাংশন কল করলে আউটপুটে আসবে Good Morning! Have a great day!
+function greetMe () {
+    console.log('Good Morning! Have a great day!')
+}
+greetMe()
+
+
+
+
+// ৬. তোর সবচেয়ে ক্লোজ তিন বন্ধুর নাম দেখানোর জন্য getFavFriends নামে ফাংশন লিখ। তারপর সেটার ভিতরে তোর ক্লোজ তিন বন্ধুর নাম আউটপুট হিসেবে দেখাবি।
+function getFavFriends () {
+    console.log('1. Tarek')
+    console.log('2. Shihab')
+    console.log('3. Rahat')
+}
+getFavFriends()
+
+
+
+// ৭. introduceMyself নামে ফাংশন লিখ। ফাংশন কল করলে তোর নাম, বয়স, ঠিকানা, ফোন নাম্বার, উচ্চতা আর তোের প্রিয় খাবার আউটপুট হিসেবে দেখা।
+function introduceMyself () {
+    console.log('Name: Md. Mahdi Hassan Rafi')
+    console.log('Age: 21')
+    console.log('Address: Lakshmipur')
+    console.log('Phone: 015********')
+    console.log('Height: 5 feet 7 inches')
+    console.log('Favorite Food: Biriyani')
+}
+introduceMyself()
