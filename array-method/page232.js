ce6ee6de9bbcf6c5a581a7379f3d02accb025f20
Mahdi Hassan <mahdioffi333@gmail.com>
2025-04-20 15:18:01 +0600

    page 233 practice problem solved

--- array-method/page232.js
+++ array-method/page232.js
@@ -17,7 +17,11 @@ console.log(numbersDescending);
 
 
 // ৩. বন্ধুদের age অনুসারে sorting করে দেখা const friends = [{name: 'Ali', age: 29}, {name: 'Sara', age: 22}, {name: 'Tariq', age: 35}];
-const friends = [{name: 'Ali', age: 29}, {name: 'Sara', age: 22}, {name: 'Tariq', age: 35}];
+const friends = [
+    {name: 'Ali', age: 29},
+    {name: 'Sara', age: 22},
+    {name: 'Tariq', age: 35}
+];
 friends.sort((a, b) => a.age - b.age)
 console.log(friends);
 
