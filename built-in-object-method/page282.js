d9b6ca479f527417802229d5cafe04f0c70a05ca
Mahdi Hassan <mahdioffi333@gmail.com>
2025-04-28 14:18:12 +0600

    page 282 practice problem solved

---
+++ built-in-object-method/page282.js
@@ -0,0 +1,31 @@
+// 4 practice problem
+
+// ১. একটা ভেরিয়েবল বানা, যার নাম হবে start। এর ভ্যালু হবে '786'। এরপর এই স্ট্রিংটাকে সংখ্যায় রূপান্তর কর।
+const start = '786'
+const startNumber = +start
+console.log(startNumber)
+
+
+
+
+// ২. একটা ভেরিয়েবল বানা, যার নাম willAttend এবং এইটার মান ০ হবে। এখন এই ভেরিয়েবলকে Boolean টাইপে কনভার্ট কর এবং console-এ দেখা।
+const willAttend = 0
+const booleanValue = Boolean(willAttend)
+console.log(booleanValue)
+
+
+
+
+// ৩. Object কন্সট্রাক্টর দিয়ে একটা অবজেক্ট তৈরি কর, যেখানে city এবং country নামে দুইটা প্রোপার্টি থাকবে।
+const location = new Object()
+location.city = 'Dhaka'
+location.country = 'Bangladesh'
+console.log(location)
+
+
+
+
+// ৪. একটা ভেরিয়েবল বানা, যার নাম হবে total। এর ভ্যালু হবে 1970। এই সংখ্যাটাকে স্ট্রিংয়ে রূপান্তর কর।
+const total = 1970
+const totalString = total + ''
+console.log(totalString)
